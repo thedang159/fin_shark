66a1825b88e02a3fc9b29764eedf874aea36888e
the.dang <the.dang@pionero.io>
2024-04-15 14:48:49 +0700

    add pagination

--- Repositories/StockRepository.cs
+++ Repositories/StockRepository.cs
@@ -51,8 +51,14 @@ namespace api.Repositories
                         : stocks.OrderBy(c => c.Symbol);
                 }
             }
+            else
+            {
+                stocks = stocks.OrderBy(c => c.Symbol);
+            }
 
-            return await stocks.Include(c => c.Comments).ToListAsync();
+            var skipNumber = (query.PageNumber - 1) * query.PageSize;
+
+            return await stocks.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(string id)
